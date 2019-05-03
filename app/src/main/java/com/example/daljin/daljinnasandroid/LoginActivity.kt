fe0905f84cc864e5a0cb8d2a02b00302723e9e73
JangDaljin <toyyj15@naver.com>
2019-05-04 00:45:18 +0900

    VER 0.0.0

--- app/src/main/java/com/example/daljin/daljinnasandroid/LoginActivity.kt
+++ app/src/main/java/com/example/daljin/daljinnasandroid/LoginActivity.kt
@@ -1,14 +1,11 @@
 package com.example.daljin.daljinnasandroid
 
-import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.inputmethod.EditorInfo
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
-import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 
 class LoginActivity : AppCompatActivity() {
@@ -16,6 +13,34 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        edt_ID.setOnEditorActionListener {
+            textView , actionId , keyEvent ->
+            when(actionId) {
+                EditorInfo.IME_ACTION_NEXT -> {
+                    edt_PW.requestFocus()
+                }
+                else -> {
+                    Log.d("DALJIN" , "$actionId")
+                    return@setOnEditorActionListener false
+                }
+            }
+            return@setOnEditorActionListener true
+        }
+
+        edt_PW.setOnEditorActionListener {
+            v, actionId, event ->
+            when(actionId) {
+                EditorInfo.IME_ACTION_DONE -> {
+                    btn_Login.performClick()
+                }
+                else -> {
+                    return@setOnEditorActionListener false
+                }
+            }
+            return@setOnEditorActionListener true
+        }
 
         btn_Login.setOnClickListener {
                 DaljinNodeWebLogin(this@LoginActivity, edt_ID.text.toString(), edt_PW.text.toString())
@@ -29,7 +54,12 @@ class LoginActivity : AppCompatActivity() {
                     }
                 }
         }
-        btn_Signup.setOnClickListener {  } //미구현
+        btn_Signup.setOnClickListener {
+
+
+
+
+        }
     }
 }
 
--- app/src/main/java/com/example/daljin/daljinnasandroid/SplashActivity.kt
+++ app/src/main/java/com/example/daljin/daljinnasandroid/SplashActivity.kt
@@ -3,6 +3,11 @@ package com.example.daljin.daljinnasandroid
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
 
@@ -10,11 +15,15 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Thread().run{
-            Thread.sleep(1500)
+
+        splashLogo.startAnimation(AnimationUtils.loadAnimation(this@SplashActivity , R.anim.showlogo))
+
+        GlobalScope.launch {
+            delay(2000)
             startActivity(Intent(this@SplashActivity , FileActivity::class.java))
             finish()
         }
+
 
         //startActivity(Intent(this@SplashActivity , TestActivity::class.java))
     }
