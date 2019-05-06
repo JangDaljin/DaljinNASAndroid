dca012660a8c60224fe04ca148f69cafdf0d925a
JangDaljin <toyyj15@naver.com>
2019-05-06 16:15:45 +0900

    VER 1.0.1 웹서버 함수 통합버전

--- app/src/main/java/com/daljin/daljinnasandroid/CommonModule.kt
+++ app/src/main/java/com/daljin/daljinnasandroid/CommonModule.kt
@@ -38,40 +38,40 @@ const val SAVE_IGNORE = 2
 const val SERVER_URL = "http://daljin.dlinkddns.com" // RELEASE
 //const val SERVER_URL = "http://10.0.2.2:8000"      // TEST
 
-const val URL_LOGIN ="/loginNW"
+const val URL_LOGIN ="/login"
 const val FORM_NAME_LOGIN_ID = "ID"
 const val FORM_NAME_LOGIN_PW = "PW"
 
 
-const val URL_FILELIST = "/fileList"
+const val URL_FILELIST = "/filelist"
 const val FORM_NAME_FILELIST_PATH = "path"
 
-const val URL_LOGOUT = "/logoutNW"
+const val URL_LOGOUT = "/logout"
 
-const val URL_DOWNLOAD = "/Download"
+const val URL_DOWNLOAD = "/download"
 const val FORM_NAME_DOWNLOAD_PATH = "n_itemPath"
 const val FORM_NAME_DOWNLOAD_ITEM = "n_downloadItem"
 const val FORM_NAME_DOWNLOAD_TYPE = "n_itemType"
 
-const val URL_MKDIR = "/makeDirectoryNW"
-const val FORM_NAME_MKDIR_PATH = "n_makeDirectory_path"
-const val FORM_NAME_MKDIR_NAME = "n_makeDirectory_Name"
+const val URL_MKDIR = "/mkdir"
+const val FORM_NAME_MKDIR_PATH = "mkdirPath"
+const val FORM_NAME_MKDIR_NAME = "mkdirName"
 
 
-const val URL_DELETE ="/DeleteNW"
-const val FORM_NAME_DELETE_PATH = "n_deletePath"
-const val FORM_NAME_DELETE_LIST = "n_deleteList"
+const val URL_DELETE ="/delete"
+const val FORM_NAME_DELETE_PATH = "daletePath"
+const val FORM_NAME_DELETE_LIST = "deleteList"
 
 
 const val URL_CHECKID = "/checkid"
 const val FORM_NAME_CHECKID_ID = "ID"
 
-const val URL_ADDUSER = "/adduserNW"
+const val URL_ADDUSER = "/adduser"
 const val FORM_NAME_ADDUSER_ID = "ID"
 const val FORM_NAME_ADDUSER_PW ="PW"
 const val FORM_NAME_ADDUSER_CODE ="CODE"
 
 
-const val URL_UPLOAD = "/fileUpload"
+const val URL_UPLOAD = "/upload"
 const val FORM_NAME_UPLOAD_PATH = "n_upload_path"
 
